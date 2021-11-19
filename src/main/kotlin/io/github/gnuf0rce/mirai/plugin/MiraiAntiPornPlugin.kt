fb5e24d38f8ce4108d44ca0db5a96d7bfc875603
cssxsh <cssxsh@gmail.com>
2021-11-19 16:47:52 +0800

    build: 1.0.1

--- src/main/kotlin/io/github/gnuf0rce/mirai/plugin/MiraiAntiPornPlugin.kt
+++ src/main/kotlin/io/github/gnuf0rce/mirai/plugin/MiraiAntiPornPlugin.kt
@@ -4,7 +4,7 @@ import net.mamoe.mirai.console.plugin.jvm.*
 import net.mamoe.mirai.event.*
 
 object MiraiAntiPornPlugin : KotlinPlugin(
-    JvmPluginDescription("io.github.gnuf0rce.anti-porn", "1.0.0") {
+    JvmPluginDescription("io.github.gnuf0rce.anti-porn", "1.0.1") {
         name("anti-porn")
         author("cssxsh")
     }
