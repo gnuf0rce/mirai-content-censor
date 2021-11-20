4b3484195afcf2f6e8d28ddbdcf06d4a8dd7218b
cssxsh <cssxsh@gmail.com>
2021-11-20 16:52:53 +0800

    feat: AntiPornCensorCommand QuoteReply

--- src/main/kotlin/io/github/gnuf0rce/mirai/plugin/command/AntiPornCensorCommand.kt
+++ src/main/kotlin/io/github/gnuf0rce/mirai/plugin/command/AntiPornCensorCommand.kt
@@ -2,6 +2,7 @@ package io.github.gnuf0rce.mirai.plugin.command
 
 import io.github.gnuf0rce.mirai.plugin.*
 import net.mamoe.mirai.console.command.*
+import net.mamoe.mirai.message.data.*
 
 object AntiPornCensorCommand: SimpleCommand(
     owner = MiraiAntiPornPlugin,
@@ -11,7 +12,10 @@ object AntiPornCensorCommand: SimpleCommand(
 
     @Handler
     suspend fun CommandSenderOnMessage<*>.handle() {
-        val result = censor(fromEvent.message)
-        sendMessage("${result?.conclusion}: ${result?.render() ?: "没有可以解析的内容"}")
+        val message = with(fromEvent) {
+            message.findIsInstance<QuoteReply>()?.source?.originalMessage ?: message
+        }
+        val result = censor(message)
+        sendMessage("${result?.conclusion}: ${result?.render()}")
     }
 }
