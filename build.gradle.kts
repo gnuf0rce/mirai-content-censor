56139df08a3fc14807d0613798da6230e94f689f
cssxsh <cssxsh@gmail.com>
2022-02-03 00:14:51 +0800

    build: mirai 2.10.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.5.31"
-    kotlin("plugin.serialization") version "1.5.31"
-    id("net.mamoe.mirai-console") version "2.9.0-M1"
+    kotlin("jvm") version "1.6.0"
+    kotlin("plugin.serialization") version "1.6.0"
+    id("net.mamoe.mirai-console") version "2.10.0-RC2"
     id("net.mamoe.maven-central-publish") version "0.7.0"
 }
 
@@ -19,37 +19,26 @@ mavenCentralPublish {
 
 mirai {
     configureShadow {
-        exclude {
-            it.path.startsWith("kotlin") || it.path.startsWith("kotlinx")
-        }
-        exclude {
-            val features = listOf("auth", "compression", "json")
-            it.path.startsWith("io/ktor") && features.none { f -> it.path.startsWith("io/ktor/client/features/$f") }
-        }
-        exclude {
-            it.path.startsWith("okhttp3")
-        }
-        exclude {
-            it.path.startsWith("okio")
-        }
-        exclude {
-            it.path.startsWith("org")
-        }
+        exclude("module-info.class")
     }
 }
 
 repositories {
     mavenLocal()
-    maven(url = "https://maven.aliyun.com/repository/central")
     mavenCentral()
-    maven(url = "https://maven.aliyun.com/repository/gradle-plugin")
     gradlePluginPortal()
 }
 
 dependencies {
-    implementation("xyz.cssxsh.baidu:baidu-oauth:2.0.4")
-    implementation("xyz.cssxsh.baidu:baidu-aip:2.0.4")
-    testImplementation(kotlin("test", "1.5.31"))
+    implementation("xyz.cssxsh.baidu:baidu-aip:2.0.5") {
+        exclude(group = "org.jetbrains.kotlin")
+        exclude(group = "org.jetbrains.kotlinx")
+        exclude(group = "org.slf4j")
+        exclude(group = "io.ktor", module = "ktor-client-core")
+        exclude(group = "io.ktor", module = "ktor-client-core-jvm")
+        exclude(group = "io.ktor", module = "ktor-client-okhttp")
+    }
+    testImplementation(kotlin("test", "1.6.0"))
 }
 
 java {
