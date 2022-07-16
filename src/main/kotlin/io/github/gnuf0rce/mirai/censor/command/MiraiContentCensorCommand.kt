96c2bb8bb2e326e4f8a5a3c1e415b1db03ba7e90
cssxsh <cssxsh@gmail.com>
2022-07-16 21:17:09 +0800

    fix: censor command

--- src/main/kotlin/io/github/gnuf0rce/mirai/censor/command/MiraiContentCensorCommand.kt
+++ src/main/kotlin/io/github/gnuf0rce/mirai/censor/command/MiraiContentCensorCommand.kt
@@ -1,8 +1,13 @@
 package io.github.gnuf0rce.mirai.censor.command
 
 import io.github.gnuf0rce.mirai.censor.*
+import net.mamoe.mirai.console.*
 import net.mamoe.mirai.console.command.*
+import net.mamoe.mirai.console.util.*
+import net.mamoe.mirai.contact.*
+import net.mamoe.mirai.message.code.*
 import net.mamoe.mirai.message.data.*
+import net.mamoe.mirai.message.*
 
 internal object MiraiContentCensorCommand : SimpleCommand(
     owner = MiraiContentCensorPlugin,
@@ -10,11 +15,21 @@ internal object MiraiContentCensorCommand : SimpleCommand(
     description = "测试内容是否合法"
 ) {
 
-    @Handler
-    suspend fun CommandSenderOnMessage<*>.handle() {
-        val message = with(fromEvent) {
-            message.findIsInstance<QuoteReply>()?.source?.originalMessage ?: message
+    private suspend fun CommandSender.request(hint: String, contact: Contact? = null): MessageChain = when (this) {
+        is ConsoleCommandSender -> {
+            val code = MiraiConsole.requestInput(hint)
+            MiraiCode.deserializeMiraiCode(code, contact)
         }
+        is CommandSenderOnMessage<*> -> {
+            sendMessage(hint)
+            fromEvent.nextMessage()
+        }
+        else -> throw IllegalStateException("未知环境 $this")
+    }
+
+    @Handler
+    suspend fun CommandSender.handle() {
+        val message = request(hint = "请输入要检查的消息")
         val results = censor(message = message)
         val reply = if (results.isEmpty()) {
             "没有可以检测的内容"
